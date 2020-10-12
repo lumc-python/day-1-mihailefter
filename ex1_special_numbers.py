053883bf5d3c90c4f4414e08bdb98629a6c1505c
Mihai <M.Lefter@lumc.nl>
2020-10-12 16:03:23 +0200

    Add solutions 1 till 4

--- ex1_special_numbers.py
+++ ex1_special_numbers.py
@@ -4,4 +4,6 @@
 # Print all the numbers between `10` and `1313` (both included) which are
 # divisible by `13` and multiple of `5`.
 
-print('hello')
+for number in range(10, 1314):
+    if (number % 13 == 0) and (number % 5 == 0):
+        print(number)
--- ex4_bank_account.py
+++ ex4_bank_account.py
@@ -6,3 +6,16 @@
 # until the user introduces `exit`. After each input, in case the balance is
 # about to go negative, print an error message (e.g., `Operation not permitted:
 # insufficient funds.`). Otherwise, print the new balance value.
+
+balance = float(input("Initial balance: "))
+
+while True:
+    action = input("Action: ")
+    if action == 'exit':
+        break
+    action = float(action)
+    if balance + action >= 0:
+        balance += action
+        print("New balance: {}".format(balance))
+    else:
+        print("Operation not permitted: insufficient funds.")
